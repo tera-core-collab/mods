ace1696920e527df5a4bb564aadceec84cb39095
pinkipi <yarnovax@gmail.com>
2019-11-08 10:10:09 -0500

    Update

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 window.app = new Vue({
 	el: '#app',
 	data: {
-		mods: []
+		mods: [],
+		lastSort: ''
 	},
-	async mounted() { this.mods = await (await fetch(`mods.json`)).json() }
+	methods: {
+		sortBy(key) {
+			const order = this.lastSort === key ? -1 : 1
+			this.mods = this.mods.sort((a, b) => a[key].localeCompare(b[key]) * order)
+			this.lastSort = order === -1 ? '' : key
+		}
+	},
+	async mounted() {
+		this.mods = await (await fetch(`mods.json`)).json()
+		this.sortBy('name')
+	}
 })
